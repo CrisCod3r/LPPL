b4c0d7af5885cbf3949aa9e61b87807b824711a2
CRIGIDE <crigide@alumno.upv.es@ldsic-vdi03.upvnet.upv.es>
2021-10-27 09:20:49 +0200

    Actualizado header.h

--- include/header.h
+++ include/header.h
@@ -6,12 +6,25 @@
 /****************************************************** Constantes generales */
 #define FALSE 0
 #define TRUE  1
+
+/***************************************************** Constantes simbolicas */
+#define TALLA_TIPO_SIMPLE 1      /* Talla asociada a los tipos simples       */
+#define TALLA_SEGENLACES  2      /* Talla del segmento de Enlaces de Control */
+
+/************************ Variables externas definidas en Programa Principal */
+extern int verTdS;                      /* Flag para saber si mostrar la TdS */
+
+/***************************** Variables externas definidas en las librerıas */
+extern int dvar;               /* Desplazamiento en el Segmento de Variables */
+extern int niv;                /* Nivel de anidamiento "global" o "local"    */
+
 /************************************* Variables externas definidas en el AL */
 extern int yylex();
 extern int yyparse();
 
 extern FILE *yyin;
 extern int   yylineno;
+
 /********************************* Funciones y variables externas auxiliares */
 extern int verbosidad;                   /* Flag si se desea una traza       */
 
